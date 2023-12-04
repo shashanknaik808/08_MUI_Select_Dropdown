cb549063c7c0527f0f891542c6495f4336af779c
Shashank Naik <shashanknaik808@gmail.com>
2023-12-04 15:18:35 +0530

    Implemented controlled form in Test component

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    TextField,
+    Button,
+    FormGroup,
+    FormControlLabel,
+    Checkbox
+} from '@mui/material';
 
 function Test() {
+
+    const [inputs, setInputs] = useState({
+        name: "",
+        email: "",
+        password: "",
+        subscribe: false
+    });
+
+    function handleChange(e) {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(inputs);
+    }
+
     return (
-        <div>Hello World</div>
+        <div>
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={handleSubmit}>
+                <TextField name='name' value={inputs.name}
+                    onChange={handleChange} type={'text'}
+                    sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
+                <TextField name='email' value={inputs.email}
+                    onChange={handleChange} type={'email'}
+                    sx={{ margin: 3 }} placeholder='Email' variant='standard' />
+                <TextField name='password' value={inputs.password}
+                    onChange={handleChange} type={'password'}
+                    sx={{ margin: 3 }} placeholder='Password' variant='filled' />
+
+                <FormGroup>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                onChange={() =>
+                                    setInputs((prev) => ({
+                                        ...prev,
+                                        subscribe: !inputs.subscribe,
+                                    }))
+                                }
+                            />
+                        } label="I'm not a Robot" />
+                </FormGroup>
+
+                <Button type='submit'>Submit</Button>
+            </form>
+        </div>
     )
 }
 
